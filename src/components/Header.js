27830d6a75e5304b7afbda718972f65fa2d5f683
rifaturrana <smrifaturrana@gmail.com>
2022-11-05 18:07:04 +0600

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import React from "react";
+
+const Header = (props) => {
+  return (
+    <Container>
+      <Content>
+        <Logo>
+          <a href="/home">
+            <img src="/images/home-logo.svg" alt="" />
+          </a>
+        </Logo>
+      </Content>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  background-color: white;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+  left: 0;
+  padding: 0 24px;
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  z-index: 100;
+`;
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+  min-height: 100%;
+  max-width: 1128px;
+`;
+const Logo = styled.span`
+  margin-right: 8px;
+  font-size: 0px;
+
+  & > a {
+    width: 135px;
+    height: 34px;
+    @media (max-width: 768px) {
+      padding: 0 5px;
+    }
+  }
+`;
+export default Header;
